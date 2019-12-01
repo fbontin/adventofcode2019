d217dc2aa5f05c850e6b845f3ab2fd6486af709d
Filip Bontin <filip.bontin@jayway.com>
2019-12-01 15:54:30 +0100

    Add solution for day 1, part 1

--- 1-1.ts
+++ 1-1.ts
@@ -1,5 +1,11 @@
 // solution for day 1, part 1 will end up here
 
-const str: string = 'hej'
+import { input } from './1-input'
 
-console.log(str)
+const calcFuel = (mass: number): number => {
+  return Math.floor(mass / 3) - 2
+}
+
+const result = input.map(m => calcFuel(m)).reduce((acc, f) => acc + f, 0)
+
+console.log('result', result)
